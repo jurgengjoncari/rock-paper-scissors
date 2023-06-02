c7c57d792b048a9a22909ab52d4cb9d1ca608039
Jurgen <jurgen.gjoncari@outlook.com>
2023-06-02 09:12:56 +0200

    change the svg size

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-shapes = ['rock', 'paper', 'scissors']
+const choices = ['rock', 'paper', 'scissors']
 
 let playerScore = 0
 let computerScore = 0
+let result
 
-function playRound(playerSelection) {
-    let result = undefined
 
-    const computerSelection = shapes[Math.floor(Math.random() * 3)]
-
-    if (playerSelection == computerSelection) {
-        result = 'Draw.'
-    } else {
-        if (
-            (playerSelection == 'rock' && computerSelection == 'paper') ||
-            (playerSelection == 'paper' && computerSelection == 'scissors') ||
-            (playerSelection == 'scissors' && computerSelection == 'rock')
-        ) {
-            result = `You lose! ${computerSelection} beats ${playerSelection}.`
-            computerScore++
-            computer.textContent = computerScore
-        } else {
-            result = `You win! ${playerSelection} beats ${computerSelection}.`
-            playerScore++
-            player.textContent = playerScore
-        }
-    }
-    resultLine.textContent = result
-}
+const playerScoreOutput = document.getElementById('playerScore')
+const computerScoreOutput = document.getElementById('computerScore')
 
 const resultLine = document.getElementById('result')
 
-const player = document.getElementById('player')
-const computer = document.getElementById('computer')
-
+// Get buttons
 const rock = document.getElementById('rock')
 const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
 
+// Event listeners for the buttons
 rock.addEventListener('click', () => playRound('rock'))
 paper.addEventListener('click', () => playRound('paper'))
 scissors.addEventListener('click', () => playRound('scissors'))
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice == computerChoice) {
+        result += "It's a tie!"
+    } else {
+        if (
+            (userChoice == 'rock' && computerChoice == 'paper') ||
+            (userChoice == 'paper' && computerChoice == 'scissors') ||
+            (userChoice == 'scissors' && computerChoice == 'rock')
+        ) {
+            result += `You lose! ${computerChoice} beats ${userChoice}.`
+            computerScore++
+            computerScoreOutput.textContent = computerScore
+        } else {
+            result += `You win! ${userChoice} beats ${computerChoice}.`
+            playerScore++
+            playerScoreOutput.textContent = playerScore
+        }
+    }
+}
+
+function playRound(userChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)]
+    result = `You chose ${userChoice}, computer chose ${computerChoice}. `
+    determineWinner(userChoice, computerChoice)
+    resultLine.textContent = result
+}
