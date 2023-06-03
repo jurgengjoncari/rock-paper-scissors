c23f4a2994f6d9bd1abcb9d757a556eeec68ac66
Jurgen <jurgen.gjoncari@outlook.com>
2023-06-03 09:38:12 +0200

    Transform the logic into a class

--- index.js
+++ index.js
@@ -1,53 +1,99 @@
-const choices = ['rock', 'paper', 'scissors']
+class RockPaperScissors {
+    CHOICES = ['rock', 'paper', 'scissors']
 
-// Get buttons
-const rock = document.getElementById('rock')
-const paper = document.getElementById('paper')
-const scissors = document.getElementById('scissors')
+    MAX_ROUNDS = 5
 
-let playerScore
-let computerScore
+    BUTTON_ELEMENTS = {
+        ROCK: document.getElementById('rock'),
+        PAPER: document.getElementById('paper'),
+        SCISSORS: document.getElementById('scissors')
+    }
 
-initialize()
+    SCORE_OUTPUT_ELEMENTS = {
+        PLAYER: document.getElementById('playerScore'),
+        COMPUTER: document.getElementById('computerScore')
+    }
 
-// Event listeners for the buttons
-rock.addEventListener('click', () => playRound('rock'))
-paper.addEventListener('click', () => playRound('paper'))
-scissors.addEventListener('click', () => playRound('scissors'))
+    playerScore
+    computerScore
+    currentRound
 
-function initialize() {
-    playerScore = 0
-    computerScore = 0
-    updateScoreOutput()
-}
+    constructor() {
+        this.initializeGame()
 
-function determineWinner(userChoice, computerChoice) {
-    if (userChoice == computerChoice) {
-        return "It's a tie!"
-    } else {
-        if (
-            (userChoice == 'rock' && computerChoice == 'paper') ||
-            (userChoice == 'paper' && computerChoice == 'scissors') ||
-            (userChoice == 'scissors' && computerChoice == 'rock')
-        ) {
-            computerScore++
-            return `You lose - ${computerChoice} beats ${userChoice}.`
+        this.BUTTON_ELEMENTS.ROCK.addEventListener('click', () => this.playGame('rock'))
+        this.BUTTON_ELEMENTS.PAPER.addEventListener('click', () => this.playGame('paper'))
+        this.BUTTON_ELEMENTS.SCISSORS.addEventListener('click', () => this.playGame('scissors'))
+    }
+
+    initializeGame() {
+        this.playerScore = 0
+        this.computerScore = 0
+        this.currentRound = 0
+        this.updateScoreOutputElements()
+    }
+
+    getComputerChoice() {
+        const RANDOM_INDEX = Math.floor(Math.random() * 3)
+        return this.CHOICES[RANDOM_INDEX]
+    }
+
+    playGame(userChoice) {
+        let computerChoice = this.getComputerChoice()
+        this.currentRound++
+
+        let result
+        if (userChoice == computerChoice) {
+            result = "It's a tie!"
         } else {
-            playerScore++
-            return `You win - ${userChoice} beats ${computerChoice}.`
+            if (
+                (userChoice == 'rock' && computerChoice == 'paper') ||
+                (userChoice == 'paper' && computerChoice == 'scissors') ||
+                (userChoice == 'scissors' && computerChoice == 'rock')
+            ) {
+                this.computerScore++
+                result = `You lose - ${computerChoice} beats ${userChoice}.`
+            } else {
+                this.playerScore++
+                result = `You win - ${userChoice} beats ${computerChoice}.`
+            }
         }
+        this.updateUI(userChoice, computerChoice, result)
+    }
+
+    getGameResult() {
+        if (this.playerScore > this.computerScore) {
+            return "Congratulations, you win this game! "
+        } else if (this.computerScore > this.playerScore) {
+            return "Computer wins the game! "
+        } else if (this.playerScore === this.computerScore) {
+            return "It's a tie game!"
+        }
+    }
+
+    updateUI(userChoice, computerChoice, result) {
+        let title = `Round ${this.currentRound}/${this.MAX_ROUNDS}`
+        let description = `You chose ${userChoice}, Computer chose ${computerChoice}.`
+
+        this.updateScoreOutputElements()
+        setTimeout(() => {
+            alert(`${title} \n\n${description} \n\n${result}`)            
+            if (this.currentRound === this.MAX_ROUNDS) {
+                title = `Game ended`
+                description = `Your final score is ${this.playerScore} and the Computer's final score is ${this.computerScore}.`
+                result = this.getGameResult()
+                setTimeout(() => {
+                    alert(`${title} \n\n${description} \n\n${result}`)
+                    this.initializeGame()                
+                }, 200);
+            }
+        }, 200);
+    }
+    
+    updateScoreOutputElements() {
+        this.SCORE_OUTPUT_ELEMENTS.PLAYER.textContent = this.playerScore
+        this.SCORE_OUTPUT_ELEMENTS.COMPUTER.textContent = this.computerScore
     }
 }
 
-function updateScoreOutput() {
-    const playerScoreOutput = document.getElementById('playerScore')
-    const computerScoreOutput = document.getElementById('computerScore')
-    playerScoreOutput.textContent = playerScore
-    computerScoreOutput.textContent = computerScore
-}
-
-function playRound(userChoice) {
-    const computerChoice = choices[Math.floor(Math.random() * 3)]
-    alert(`You chose ${userChoice}, computer chose ${computerChoice}. \n\n${determineWinner(userChoice, computerChoice)}`)
-    updateScoreOutput()
-}
+new RockPaperScissors
