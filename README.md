703d6c6d655e3db55adab8e493e8ab72ec75aa1d
jurgen <jurgen_gjoncari@icloud.com>
2026-02-11 14:34:24 +0100

    Some refactoring

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 # Rock Paper Scissors
 
-Rock-Paper-Scissors game for The Odin Project Fundamentals course.
-
-Live preview: https://jurgengjoncari.github.io/rock-paper-scissors/
-
-Instructions:
-
-1. [The logic](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors).
-2. [The interface](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/revisiting-rock-paper-scissors)
-
-Some notes:
-
-- I've excluded the part where the game stops after reaching 5 points. 
-- I've included the SVG file inside the HTML because it was the only way to add interactivity to it. 
+The classic Rock-Paper-Scissors game. 
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+CHOICES = ['rock', 'paper', 'scissors']
+
+MAX_ROUNDS = 5
+
+BUTTONS = {
+    ROCK: document.getElementById('rock'),
+    PAPER: document.getElementById('paper'),
+    SCISSORS: document.getElementById('scissors')
+}
+
+OUTPUTS = {
+    PLAYER: document.getElementById('player-score'),
+    COMPUTER: document.getElementById('computer-score'),
+    ROUND: document.getElementById('round-number'),
+}
+
 class RockPaperScissors {
-    CHOICES = ['rock', 'paper', 'scissors']
-
-    MAX_ROUNDS = 5
-
-    BUTTON_ELEMENTS = {
-        ROCK: document.getElementById('rock'),
-        PAPER: document.getElementById('paper'),
-        SCISSORS: document.getElementById('scissors')
-    }
-
-    SCORE_OUTPUT_ELEMENTS = {
-        PLAYER: document.getElementById('playerScore'),
-        COMPUTER: document.getElementById('computerScore')
-    }
-
     playerScore
     computerScore
     currentRound
@@ -21,78 +22,85 @@ class RockPaperScissors {
     constructor() {
         this.initializeGame()
 
-        this.BUTTON_ELEMENTS.ROCK.addEventListener('click', () => this.playGame('rock'))
-        this.BUTTON_ELEMENTS.PAPER.addEventListener('click', () => this.playGame('paper'))
-        this.BUTTON_ELEMENTS.SCISSORS.addEventListener('click', () => this.playGame('scissors'))
+        BUTTONS.ROCK.addEventListener('click', () => this.playGame('rock', this.computerChoice))
+        BUTTONS.PAPER.addEventListener('click', () => this.playGame('paper', this.computerChoice))
+        BUTTONS.SCISSORS.addEventListener('click', () => this.playGame('scissors', this.computerChoice))
+    }
+
+    get computerChoice() {
+        const RANDOM_INDEX = Math.floor(Math.random() * 3)
+        return CHOICES[RANDOM_INDEX]
+    }
+
+    get gameResult() {
+        if (this.playerScore > this.computerScore) {
+            return "Congratulations, you won the game! "
+        } else if (this.computerScore > this.playerScore) {
+            return "Computer won the game! "
+        } else if (this.playerScore === this.computerScore) {
+            return "The game is a tie!"
+        }
+    }
+
+    playGame(userChoice, computerChoice) {
+        let result
+
+        if (userChoice === computerChoice) {
+            result = "It's a tie!"
+        }
+        else {
+            if (
+                (userChoice === 'rock' && computerChoice === 'paper') ||
+                (userChoice === 'paper' && computerChoice === 'scissors') ||
+                (userChoice === 'scissors' && computerChoice === 'rock')
+            ) {
+                this.computerScore++
+                result = `You lose — ${computerChoice} beats ${userChoice}.`
+            }
+            else {
+                this.playerScore++
+                result = `You win — ${userChoice} beats ${computerChoice}.`
+            }
+        }
+
+        this.showAlerts(userChoice, computerChoice, result)
+
+        this.currentRound++
+    }
+
+    showAlerts(userChoice, computerChoice, result) {
+        let title = `Round ${this.currentRound}`
+
+        let description = `You chose ${userChoice}, Computer chose ${computerChoice}.`
+
+        setTimeout(() => {
+            alert(`${title} \n\n${description} \n\n${result}`)
+
+            this.updateOutputs()
+
+            if (this.currentRound > MAX_ROUNDS) {
+                title = `Game over`
+                description = `Your final score is ${this.playerScore} and the Computer's final score is ${this.computerScore}.`
+                result = this.gameResult
+                setTimeout(() => {
+                    alert(`${title} \n\n${description} \n\n${result}`)
+                    this.initializeGame()                
+                }, 500);
+            }
+        }, 200);
     }
 
     initializeGame() {
         this.playerScore = 0
         this.computerScore = 0
-        this.currentRound = 0
-        this.updateScoreOutputElements()
+        this.currentRound = 1
+        this.updateOutputs()
     }
 
-    getComputerChoice() {
-        const RANDOM_INDEX = Math.floor(Math.random() * 3)
-        return this.CHOICES[RANDOM_INDEX]
-    }
-
-    playGame(userChoice) {
-        let computerChoice = this.getComputerChoice()
-        this.currentRound++
-
-        let result
-        if (userChoice == computerChoice) {
-            result = "It's a tie!"
-        } else {
-            if (
-                (userChoice == 'rock' && computerChoice == 'paper') ||
-                (userChoice == 'paper' && computerChoice == 'scissors') ||
-                (userChoice == 'scissors' && computerChoice == 'rock')
-            ) {
-                this.computerScore++
-                result = `You lose - ${computerChoice} beats ${userChoice}.`
-            } else {
-                this.playerScore++
-                result = `You win - ${userChoice} beats ${computerChoice}.`
-            }
-        }
-        this.updateUI(userChoice, computerChoice, result)
-    }
-
-    getGameResult() {
-        if (this.playerScore > this.computerScore) {
-            return "Congratulations, you win this game! "
-        } else if (this.computerScore > this.playerScore) {
-            return "Computer wins the game! "
-        } else if (this.playerScore === this.computerScore) {
-            return "It's a tie game!"
-        }
-    }
-
-    updateUI(userChoice, computerChoice, result) {
-        let title = `Round ${this.currentRound}`
-        let description = `You chose ${userChoice}, Computer chose ${computerChoice}.`
-
-        this.updateScoreOutputElements()
-        setTimeout(() => {
-            alert(`${title} \n\n${description} \n\n${result}`)            
-            if (this.currentRound === this.MAX_ROUNDS) {
-                title = `Game ended`
-                description = `Your final score is ${this.playerScore} and the Computer's final score is ${this.computerScore}.`
-                result = this.getGameResult()
-                setTimeout(() => {
-                    alert(`${title} \n\n${description} \n\n${result}`)
-                    this.initializeGame()                
-                }, 200);
-            }
-        }, 200);
-    }
-    
-    updateScoreOutputElements() {
-        this.SCORE_OUTPUT_ELEMENTS.PLAYER.textContent = this.playerScore
-        this.SCORE_OUTPUT_ELEMENTS.COMPUTER.textContent = this.computerScore
+    updateOutputs() {
+        OUTPUTS.PLAYER.textContent = this.playerScore
+        OUTPUTS.COMPUTER.textContent = this.computerScore
+        OUTPUTS.ROUND.textContent = this.currentRound
     }
 }
 
